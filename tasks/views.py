e68197dfc08d530b6647bf27620ddca183298d39
mdonadios <mdonadio.sistemas@gmail.com>
2022-09-16 07:49:25 -0300

    errores y pep8

--- tasks/views.py
+++ tasks/views.py
@@ -4,8 +4,11 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
+
 
 def signup(request):
 
@@ -16,9 +19,16 @@ def signup(request):
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 return HttpResponse('User create successfully')
             except:
-                return HttpResponse('Username already exist')
-        return HttpResponse('Password do not match')
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Username already exist'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Password do not match'
+        })
